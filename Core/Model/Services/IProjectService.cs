b1668b1a901fe5697ad8227ab39d852feb431330
WaveTech <shawn@wtdt.com>
2010-09-26 11:43:49 -0700

    added some saving logic to the ui

---
+++ Core/Model/Services/IProjectService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WaveTech.Insight.Model.Services
+{
+	public interface IProjectService
+	{
+		Project GetProject(string filePath);
+		Project SaveProject(Project project, string filePath);
+	}
+}
---
+++ Core/Services/ProjectService.cs
@@ -0,0 +1,26 @@
+﻿using WaveTech.Insight.Model;
+using WaveTech.Insight.Model.Repositories;
+using WaveTech.Insight.Model.Services;
+
+namespace WaveTech.Insight.Services
+{
+	public class ProjectService : IProjectService
+	{
+		private readonly IProjectRepository _projectRepository;
+
+		public ProjectService(IProjectRepository projectRepository)
+		{
+			_projectRepository = projectRepository;
+		}
+
+		public Project GetProject(string filePath)
+		{
+			return _projectRepository.GetProject(filePath);
+		}
+
+		public Project SaveProject(Project project, string filePath)
+		{
+			return _projectRepository.SaveProject(project, filePath);
+		}
+	}
+}
